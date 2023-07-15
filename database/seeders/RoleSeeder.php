8646dfddbc69b80ddfd43859a554ff99046f7a9b
omarkishk <omarkishk2000@gmail.com>
2023-07-15 17:55:16 +0200

    adding files

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = ['customer','seller','admin'];
+
+        foreach ($roles as $role){
+            Role::create(['name' => $role]);
+        }
+    }
+}
