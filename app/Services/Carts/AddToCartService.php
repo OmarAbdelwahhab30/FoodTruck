f82cafc944828e907034fac574936af5dd663843
omarkishk <omarkishk2000@gmail.com>
2023-07-30 22:21:42 +0200

    adding files

--- app/Services/Carts/AddToCartService.php
+++ app/Services/Carts/AddToCartService.php
@@ -15,7 +15,7 @@ class AddToCartService extends Service
 
     public function AddToCart($request)
     {
-        $cart = isset(auth("sanctum")->user()->cart)? auth("sanctum")->user()->cart:null;
+        $cart = auth("sanctum")->user()->cart;
 
         if (!isset($cart)){
             $cart = Cart::create([
@@ -23,7 +23,7 @@ class AddToCartService extends Service
             ]);
         }
 
-        if (!$this->IsFoundInCart($request->product_id))
+        if (!$this->IsFoundInCart($request->product_id,$cart->id))
         {
             $cart->products()->attach($request->product_id,array('count' => $request->count));
         }else{
@@ -32,9 +32,9 @@ class AddToCartService extends Service
         return $cart->products;
     }
 
-    public function IsFoundInCart($product_id): bool
+    public function IsFoundInCart($product_id,$cart_id): bool
     {
-        $found = Cart_Product::where("product_id",$product_id)->first();
+        $found = Cart_Product::where("product_id",$product_id)->where("cart_id",$cart_id)->first();
         if ($found){
             return true;
         }
