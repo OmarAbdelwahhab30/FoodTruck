7094422ab8e0e3b81bfcb0498c14da86c83163c1
omarkishk <omarkishk2000@gmail.com>
2023-08-31 22:26:49 +0200

    adding files

--- lang/ar/responses.php
+++ lang/ar/responses.php
@@ -1,87 +1,57 @@
 <?php
 
 return [
-
-
     "You don't have the authorization on this action." => 'أنت لا تمتلك الصلاحية لتنفيذ هذا الأمر.',
     "Some thing went wrong ,try again later" => "حدث خطأ ما , حاول لاحقًا.",
-
-
     "User Data has been updated successfully" => "تم تحيث بيانات المستخدم بنجاح",
     "Current Password is not correct" => "كلمة المرور الحالية غير صحيحة.",
     "Password has been changed successfully" => "تم تغيير كلة المرور بنجاح.",
-
     "Here is User Data" => "هذه بيانات المستخدم",
-
     "User logged successfully" => "تم تسجيل الدخول بنجاح",
     "These inputs are invalid , try again." => "المدخلات غير صحيحة , حاول مجددًا",
     "You have been successfully logged out!" => "تم تسجيل الخروج بنجاح",
-
-
     "Product Added Successfully" => "تمت إضافة المنتج بنجاح",
-
     "Here Are Products in the cart" => "هذه منتجات العربة",
-
     "Here are the latest messages" => "أخر الرسائل",
     "Your Message has been added successfully" => "تمت إضافة رسالتك بنجاح",
-
     "Here are all food types" => "هذه هي جميع أنواع الأكلات",
-
     "Here are the nearest Trucks" => "هذه هي أقرب العربات ",
     "User must enter his location firstly" => "يجب إدخال موقعك الحالي أولًا",
     "Location has been added successfully" => "تمت إضافة موقعك بنجاح",
-
     "Here is the Order" => "هذا هو الطلب",
     "Order has been cancelled successfully" => "تم إلغاء الطلب بنجاح",
-
     "Here is Order Info" => "هذه هي معلومات الطلب",
-
     "Here are previous Orders Info" => "هذه هي الطلبات السابقة",
-
     "Here are Processing Orders Info" => "طلبات جاري العمل عليها",
-
     "Here are Pending Orders Info" => "هذه هي الطلبات المعلقة",
-
     "Here are Current Orders Info" => "معلومات عن الطلبات الحالية",
-
     "Here is payment Info" => "معلومات عن الدفع",
     "Payment has been completed successfully." => "تمت عملية الدفع بنجاح",
-
     "Product has been added successfully" => "تم إضافة المنتج بنجاح",
     "Product has been updated successfully" => "تم تحديث المنتج بنجاح",
     "Product Image has been deleted successfully" => "تم حذف الصورة بنجاح",
     "Review has been added successfully." => "تم إضافة رأيك بنجاح",
-
     "Here are all reviews." => "كل الآراء",
-
     "Here is the truck Reviews." => "كل الآراء عن العربة",
     "There is no reviews to show." => "لا يوجد آراء لعرضها",
-
     "Food Type has been added successfully" => "تم إضافة النوع بنجاح",
-
     "Here are All Products inside this Section." => "جميع المنتجات داخل القسم",
-
     "Here are All Section inside this truck." => "جميع الأقسام داخل العربة",
     "There is no products to show" => "لا يوجد منتجات لعرضها",
     "There is no sections to show" => "لا يوجد أقسام لعرضها",
     "Food Type has been updated successfully" => "تم تحديث النوع بنجاح",
     "Truck Image has been deleted successfully" => "تم حذف الصورة بنجاح",
-
     "Here are all products" => "كل المنتجات",
     "There is no trucks to show" => "لا يوجد عربات لعرضها",
-
     "Here are All Trucks." => "كل العربات",
-
     "Here is the truck information." => "معلومات عن العربة",
     "There is no details" => "لا يوجد تفاصيل",
     "Truck information Has been updated successfully" => "تم تحديث المعلومات بنجاح",
     "Truck delivery status has been updated successfully." => "تم تحديث حالة التوصيل بنجاح ",
     "Product has been removed successfully." => "تم إزالة المنتج بنجاح",
-
     "Please , choose your products from the same truck !" => "من فضلك أدخل المنتجات من نفس عربة التسوق !",
     "Bank Account Information has been added successfully." => "تم إضافة بيانات حسابك البنكي بنجاح ",
     "No information to show" => "لا يوجد معلومات لإظهارها",
-
     "notification has been saved successfully."=>"تم حفظ الإشعار بنجاح",
     "no notifications are available"=>"لا يوجد إشعارات",
     "message is here"=>"الرسالة هنا",
@@ -96,5 +66,12 @@ return [
     "Product size has been deleted successfully"=>"تم الحذف بنجاح",
     "You are not accepted yet, try again later" => "لم يتم قبولك حتى الآن , حاول لاحقًا",
     "Your account is deactivated for some reason !" => "تم إلغاء تنشيط حسابك لسببٍ ما ",
-    "Card information has been deleted successfully."   => "تم حذف البيانات بنجاح"
+    "Card information has been deleted successfully."   => "تم حذف البيانات بنجاح",
+    "Order is accepted from seller and it is being prepared now"    => "تم قبول الطلب من البائع,ويتم تجهيزه الآن",
+    "Order is rejected from seller."
+    => "تم رفض الطلب من البائع",
+    "Order is Delivered Successfully."
+    => "تم توصيل الطلب بنجاح",
+    "Order is Picked Up ."
+    => "تم استلام الطلب بنجاح",
 ];
--- lang/en/responses.php
+++ lang/en/responses.php
@@ -1,104 +1,70 @@
 <?php
 
 return [
-
     "You don't have the authorization on this action." => "You don't have the authorization on this action.",
     "Some thing went wrong ,try again later"           => "Some thing went wrong ,try again later",
-
-
     "User Data has been updated successfully"          =>  "User Data has been updated successfully",
     "Current Password is not correct"                  =>  "Current Password is not correct"    ,
     "Password has been changed successfully"           =>  "Password has been changed successfully" ,
-
     "Here is User Data"                                => "Here is User Data"     ,
-
     "User logged successfully"                         =>  "User logged successfully" ,
     "These inputs are invalid , try again."            => "These inputs are invalid , try again."   ,
     "You have been successfully logged out!"           => "You have been successfully logged out!"    ,
-
-
     "Product Added Successfully"                       => "Product Added Successfully"  ,
-
     "Here Are Products in the cart"                    =>  "Here Are Products in the cart",
-
     "Here are the latest messages"                     => "Here are the latest messages",
     "Your Message has been added successfully"         => "Your Message has been added successfully",
-
     "Here are all food types"                          => "Here are all food types" ,
-
     "Here are the nearest Trucks"                      => "Here are the nearest Trucks",
     "User must enter his location firstly"             => "User must enter his location firstly",
     "Location has been added successfully"             => "Location has been added successfully" ,
-
     "Here is the Order"                                => "Here is the Order",
     "Order has been cancelled successfully"            => "Order has been cancelled successfully",
-
     "Here is Order Info"                               => "Here is Order Info",
-
     "Here are previous Orders Info"                    => "Here are previous Orders Info",
-
     "Here are Processing Orders Info"                  => "Here are Processing Orders Info",
-
     "Here are Pending Orders Info"                     => "Here are Pending Orders Info",
-
     "Here are Current Orders Info"                     => "Here are Current Orders Info",
-
     "Here is payment Info"                             => "Here is payment Info"  ,
     "Payment has been completed successfully."         => "Payment has been completed successfully.",
-
     "Product has been added successfully"              => "Product has been added successfully" ,
     "Product has been updated successfully"            => "Product Image has been deleted successfully",
     "Product Image has been deleted successfully"      => "Product Image has been deleted successfully",
     "Product has been removed successfully."            => "Product has been removed successfully.",
     "Review has been added successfully."              => "Review has been added successfully." ,
-
     "Here are all reviews."                            =>  "Here are all reviews.",
-
     "Here is the truck Reviews."                       =>  "Here is the truck Reviews.",
     "There is no reviews to show."                     => "There is no reviews to show.",
-
     "Food Type has been added successfully"            =>  "Food Type has been added successfully",
-
     "Here are All Products inside this Section."       => "Here are All Products inside this Section.",
-
     "Here are All Section inside this truck."          => "Here are All Section inside this truck."  ,
     "There is no products to show"                     => "There is no products to show" ,
     "There is no sections to show"                     =>  "There is no sections to show"  ,
     "Food Type has been updated successfully"          =>  "Food Type has been updated successfully" ,
     "Truck Image has been deleted successfully"        => "Truck Image has been deleted successfully" ,
-
     "Here are all products"                            =>  "Here are all products" ,
     "There is no trucks to show"                       =>  "There is no trucks to show",
-
     "Here are All Trucks."                             => "Here are All Trucks.",
-
     "Here is the truck information."                   => "Here is the truck information.",
     "There is no details"                              => "There is no details",
     "Truck information Has been updated successfully"  => "Truck information Has been updated successfully",
     "Truck delivery status has been updated successfully."   => "Truck delivery status has been updated successfully.",
-
     "Order is accepted from seller and it is being prepared now"
     => "Order is accepted from seller and it is being prepared now",
-
     "Order is rejected from seller."
     => "Order is rejected from seller.",
-
     "Order is Delivered Successfully."
     => "Order is Delivered Successfully.",
-
     "Order is Picked Up ."
     => "Order is Picked Up .",
-
     "Please , choose your products from the same truck !" => "Please , choose your products from the same truck !",
     "Bank Account Information has been added successfully." => "Bank Account Information has been added successfully.",
     "No information to show" => "No information to show",
-
     "notification has been saved successfully."=>"Notification has been saved successfully.",
     "no notifications are available"=>"No notifications are available",
     "message is here"=>"message is here",
     "Something went wrong, Check your inputs well !"=>"Something went wrong, Check your inputs well !",
-    "Request Has been sent to admin successfully ,wait for bank response ."
-    =>"Request has been sent to admin successfully ,wait for bank response .",
+    "Request Has been sent to admin successfully ,wait for bank response ." =>"Request has been sent to admin successfully ,wait for bank response .",
     "Product optional has been deleted successfully"=>"Product optional has been deleted successfully",
     "Product has been deleted successfully"=>"Product has been deleted successfully",
     "Card information has been added successfully."=>"Card information has been added successfully.",
