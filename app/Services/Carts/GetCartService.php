0822e4cc986e40d28bf709673e9f29d714fa9f71
omarkishk <omarkishk2000@gmail.com>
2023-09-01 02:29:39 +0200

    adding files

--- app/Services/Carts/GetCartService.php
+++ app/Services/Carts/GetCartService.php
@@ -16,18 +16,14 @@ class GetCartService extends Service
     public function GetCart($request): \Illuminate\Database\Eloquent\Collection|array
     {
         $user = auth("sanctum")->user();
-        $delivery = $user->truck->delivery;
         $cart =  Cart::with(["products" => function($q){
             $q->select("truck_id","name","products.id");
-
             $q->with("images" , function ($qq){
                 $qq->select("*");
             });
-
-//            $q->with("truck",function ($qqq){
-//                $qqq->select("id");
-//            });
+            $q->with("truck",function ($qqq){
+               $qqq->select("id","delivery");
+            });
         }])->where("id",$request->cart_id)->where("user_id",$user->id)->select("id")->get();
-    return $cart->merge($delivery);
     }
 }
