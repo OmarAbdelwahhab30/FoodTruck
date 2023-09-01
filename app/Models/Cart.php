5c3d3e38cabe76a50a93ec8c9fafabde27f54fd1
omarkishk <omarkishk2000@gmail.com>
2023-09-01 02:39:49 +0200

    adding files

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -18,7 +18,7 @@ class Cart extends Model
     public function products(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Product::class,"cart_product","cart_id","product_id")
-            ->withPivot(["id",'count','optional_price',"total_price","optional","size_id"]);
+            ->withPivot(["id",'count','optional_price',"total_price","optional","size_id","product_id","cart_id"]);
     }
 
     public function user(): \Illuminate\Database\Eloquent\Relations\HasOne
