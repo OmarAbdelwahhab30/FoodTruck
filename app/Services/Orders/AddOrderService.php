9e0ac62c1fe3e8fe7177b74a46e11013f353f047
omarkishk <omarkishk2000@gmail.com>
2023-09-01 02:51:24 +0200

    adding files

--- app/Services/Orders/AddOrderService.php
+++ app/Services/Orders/AddOrderService.php
@@ -17,56 +17,58 @@ class AddOrderService extends Service
 {
 
 
-        public function ExecTransaction($request)
-        {
-            $seller = Truck::find($request->truck_id)->user;
-            return DB::transaction(function () use ($request,$seller) {
+    public function ExecTransaction($request)
+    {
+        $seller = Truck::find($request->truck_id)->user;
+        return DB::transaction(function () use ($request, $seller) {
 
-                $order = $this->addOrder($request);
-                $this->attachProduct($request->products, $order);
-                broadcast(new OrderHasAdded($order,$order->user))->toOthers();
-                $this->DestroyCart(auth("sanctum")->user());
-                $this->addPriceToSellerWallet($request->total_price,$seller->id);
-                //$this->PushNotification($seller->player_id,1,$seller->id,auth("sanctum")->user()->name);
-                //$seller->notify(new SellerOrderNotification(auth("sanctum")->user()->name));
-                return Order::where("id", $order->id)->with("products")->get();
-            });
-        }
+            $order = $this->addOrder($request);
+            $this->attachProduct($request->products, $order);
+            broadcast(new OrderHasAdded($order, $order->user))->toOthers();
+            $this->DestroyCart(auth("sanctum")->user());
+            $this->addPriceToSellerWallet($request->total_price, $seller->id);
+            //$this->PushNotification($seller->player_id,1,$seller->id,auth("sanctum")->user()->name);
+            //$seller->notify(new SellerOrderNotification(auth("sanctum")->user()->name));
+            return Order::where("id", $order->id)->with("products")->get();
+        });
+    }
 
 
     public function addOrder($request)
     {
         return Order::create([
-            'truck_id'      => $request->truck_id ,
-            'arrival_time'  => $request->arrival_time,
+            'truck_id' => $request->truck_id,
+            'arrival_time' => $request->arrival_time,
             'delivery_type_en' => $request->delivery_type_en,
             'delivery_type_ar' => $request->delivery_type_ar,
-            'total_price'   => $request->total_price,
-            'user_id'       => auth("sanctum")->user()->id,
+            'total_price' => $request->total_price,
+            'user_id' => auth("sanctum")->user()->id,
         ]);
     }
 
-    public function attachProduct($products,$order)
+    public function attachProduct($products, $order)
     {
         foreach ($products as $product)
-        $order->products()->attach($product['id'], [
-            'size_id'     => $product['size_id'],
-            'optional'    => $product['optional'],
-            'count'       => $product['count'],
-        ]);
+            $order->products()->attach($product['id'], [
+                'size_id' => $product['size_id'],
+                'optional' => $product['optional'],
+                'count' => $product['count'],
+            ]);
     }
 
     private function DestroyCart($user)
     {
         if (isset($user->cart)) {
             $cart = Cart::find($user->cart->id);
-                $cart->products()->detach();
-            }
+            $cart->products()->detach();
+            $cart->truck_id = null;
+            $cart->save();
+        }
     }
 
-    private function addPriceToSellerWallet($price,$seller_id)
+    private function addPriceToSellerWallet($price, $seller_id)
     {
-        Wallet::where('user_id',$seller_id)->increment('balance',$price);
+        Wallet::where('user_id', $seller_id)->increment('balance', $price);
     }
 
 }
