3d4fc83813abbe2ab0fbb8ddd0a59fe745e982e1
omarkishk <omarkishk2000@gmail.com>
2023-09-03 01:36:20 +0200

    adding files

--- resources/views/admin/includes/app.blade.php
+++ resources/views/admin/includes/app.blade.php
@@ -17,6 +17,8 @@ if (LaravelLocalization::getCurrentLocale() == "ar") {
         $z = isset($z)&& $z !=null ? $z:"";
         ?>
     <title>{{__("admin.Dashboard") . $z ?? ""}}</title>
+    <link rel="icon" type="image/gif" href="{{asset("assets/images/favicon.ico")}}">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
     <meta content="Themesbrand" name="author"/>
