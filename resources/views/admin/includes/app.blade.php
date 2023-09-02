b75b4b1adf2f99f41707b0fccd8521c484d904c2
omarkishk <omarkishk2000@gmail.com>
2023-09-02 18:09:15 +0200

    adding files

--- resources/views/admin/includes/app.blade.php
+++ resources/views/admin/includes/app.blade.php
@@ -26,12 +26,6 @@ if (LaravelLocalization::getCurrentLocale() == "ar") {
 </style>
     <link rel="shortcut icon" href="{{asset("assets/images/favicon.ico")}}">
     @yield("addition")
-    <!-- Bootstrap Css -->
-    <link href="{{asset("assets/css/bootstrap.min.css")}}" id="bootstrap-style" rel="stylesheet" type="text/css"/>
-    <!-- Icons Css -->
-    <link href="{{asset("assets/css/icons.min.css")}}" rel="stylesheet" type="text/css"/>
-    <!-- App Css-->
-    <link href="{{asset("assets/css/app.min.css")}}" id="app-style" rel="stylesheet" type="text/css"/>
 
     <link rel="shortcut icon" href="{{asset("assets/images/favicon.ico")}}">
 
@@ -44,15 +38,12 @@ if (LaravelLocalization::getCurrentLocale() == "ar") {
           rel="stylesheet" type="text/css"/>
 
     <!-- Bootstrap Css -->
-    {{--    <link href="{{asset("assets/css/bootstrap.min.css")}}" id="bootstrap-style" rel="stylesheet" type="text/css"/>--}}
     <link href="{{asset("assets/css/bootstrap$x.min.css")}}" id="bootstrap-style" rel="stylesheet" type="text/css"/>
 
     <!-- Icons Css -->
-    {{--    <link href="{{asset("assets/css/icons.min.css")}}" rel="stylesheet" type="text/css"/>--}}
     <link href="{{asset("assets/css/icons$x.min.css")}}" rel="stylesheet" type="text/css"/>
 
     <!-- App Css-->
-    {{--    <link href="{{asset("assets/css/app.min.css")}}" id="app-style" rel="stylesheet" type="text/css"/>--}}
     <link href="{{asset("assets/css/app$x.min.css")}}" id="app-style" rel="stylesheet" type="text/css"/>
 
 </head>
