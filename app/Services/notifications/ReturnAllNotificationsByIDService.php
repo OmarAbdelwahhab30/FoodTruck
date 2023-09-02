abf4f90c50d677c8cdf2e50c92294343fe379a1f
omarkishk <omarkishk2000@gmail.com>
2023-09-02 23:15:22 +0200

    adding files

--- app/Services/notifications/ReturnAllNotificationsByIDService.php
+++ app/Services/notifications/ReturnAllNotificationsByIDService.php
@@ -10,7 +10,9 @@ class ReturnAllNotificationsByIDService extends \App\Services\Service
     public function ReturnNotificationsByUserID()
     {
         $user_id = auth("sanctum")->user()->id;
-        return User_Notification::where("user_id",$user_id)->with("sender")
+        return User_Notification::where("user_id",$user_id)->with("sender",function ($q){
+            $q->select("id","name","image");
+        })
             ->select("id","notification_".app()->getLocale()." as notification","created_at","sender_id")
             ->orderBy("created_at","ASC")
             ->get();
