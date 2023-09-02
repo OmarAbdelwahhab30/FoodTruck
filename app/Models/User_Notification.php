c901b6cd2392013a7fbfc732dec8b0b63cddcee7
omarkishk <omarkishk2000@gmail.com>
2023-09-02 17:13:46 +0200

    adding files

--- app/Models/User_Notification.php
+++ app/Models/User_Notification.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +13,10 @@ class User_Notification extends Model
     protected $guarded = [];
     protected $table = "users_notifications";
 
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('D, d M  H:i a');
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Services/notifications/ReturnAllNotificationsByIDService.php
+++ app/Services/notifications/ReturnAllNotificationsByIDService.php
@@ -11,7 +11,7 @@ class ReturnAllNotificationsByIDService extends \App\Services\Service
     {
         $user_id = auth("sanctum")->user()->id;
         return User_Notification::where("user_id",$user_id)
-            ->select("id","notification_".app()->getLocale()." as notification")
+            ->select("id","notification_".app()->getLocale()." as notification","created_at")
             ->orderBy("created_at","ASC")
             ->get();
     }
