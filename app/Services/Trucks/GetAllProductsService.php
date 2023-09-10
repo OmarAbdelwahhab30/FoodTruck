a057cb50b3ecdecc37974c7796c15b5fbcf5c5cb
omarkishk <omarkishk2000@gmail.com>
2023-09-10 05:53:39 +0200

    adding files

--- app/Services/Trucks/GetAllProductsService.php
+++ app/Services/Trucks/GetAllProductsService.php
@@ -17,10 +17,7 @@ class GetAllProductsService extends \App\Services\Service
         $products = Section::where("truck_id", $request->truck_id)->with("products",function ($q){
             $q->with("images");
             $q->with("sizes",function ($q) {
-                $queryOrder = "CASE WHEN size = 'small' THEN 1 ";
-                $queryOrder .= "WHEN size = 'medium' THEN 2 ";
-                $queryOrder .= "ELSE 3 END";
-                $q->orderByRaw($queryOrder, "ASC");
+                $q->orderBy("price", "ASC");
             });
             $q->with("optionals");
         })->get();
